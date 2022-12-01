f1e05f16e82d4195e88b64a4898dde882529e125
Aleksey <J2k_pot@mail.ru>
2022-12-01 21:13:10 +0300

    fix 3

--- Task/Example 3.py
+++ Task/Example 3.py
@@ -3,4 +3,4 @@ a = int(input('Введите размер списка '))
 lst = [round(random.random(), 2) for i in range(a)]
 lst2 = [round(i % 1, 2) for i in lst if i % 1 != 0]
 answer = max(lst2) - min(lst2)
-print(f'{lst} разница равна {answer}')
+print(f'{lst} разница равна {round(answer, 2)}')
