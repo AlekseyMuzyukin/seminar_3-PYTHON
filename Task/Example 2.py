3f059ad2ee44781d2932b62856817a799f564e3f
Aleksey <J2k_pot@mail.ru>
2022-12-01 20:27:06 +0300

    add 2

---
+++ Task/Example 2.py
@@ -0,0 +1,6 @@
+from  random import randint
+a =int (input('Введите размер списка '))
+lst = [randint(0, 10) for i in range(a)]
+temp = len(lst) // 2 + 1 if len(lst) % 2 != 0 else len(lst) // 2
+lst_2 = [lst[i] * lst[len(lst) - i - 1] for i in range(temp)]
+print(lst, lst_2)
